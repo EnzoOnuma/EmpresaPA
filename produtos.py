9ba7facfe6e3c35f94baf83abd62825dc211955a
Matheus <matheushgeroldo@gmail.com>
2026-09-22 19:00:33 -0300

    feat: criando a função principal, o menu e o arquivo texto

--- produtos.py
+++ produtos.py
@@ -1,1 +1,11 @@
 # Dupla: João Pedro Formiga Baptista e Matheus Henriques Geroldo - Turma B 1ºDS
+import os
+
+ARQUIVO_PRODUTOS = "produtos.txt"
+
+def executar_produtos():
+    def menu():
+        print("\n===== MENU DE PRODUTOS =====")
+        print("1 - Cadastrar produto")
+        print("2 - Listar produtos")
+        print("0 - Voltar ao menu principal")
