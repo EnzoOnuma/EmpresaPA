4679f8adae26e96242d55979dbdad95469c386b5
Matheus <matheushgeroldo@gmail.com>
2026-09-22 19:15:29 -0300

    feat: criando a função carregar_produtos (trás os nomes do arquivo texto para uma lista)

--- produtos.py
+++ produtos.py
@@ -9,3 +9,22 @@ def executar_produtos():
         print("1 - Cadastrar produto")
         print("2 - Listar produtos")
         print("0 - Voltar ao menu principal")
+
+    def carregar_produtos():
+        produtos = []
+
+        if not os.path.exists(ARQUIVO_PRODUTOS):
+            return produtos
+
+        with open(ARQUIVO_PRODUTOS, "r", encoding="utf-8") as arquivo:
+            linhas_brutas = arquivo.readlines()
+
+        for linha_bruta in linhas_brutas:
+            linha = linha_bruta.strip()
+            try:
+                nome, preco = linha.split(";")
+                produtos.append((nome, float(preco)))
+            except ValueError:
+                print(f"Linha inválida ignorada: {linha}")
+
+        return produtos
